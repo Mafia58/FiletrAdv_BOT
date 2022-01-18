70855c4f563be48f9986b14635998770896c50af
Nimesh Akalanka <90953455+Mafia58@users.noreply.github.com>
2022-01-18 07:22:04 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/EvamariaTG/EvaMaria.git /EvaMaria
+  git clone https://github.com/Mafia58/FiletrAdv_BOT
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /EvaMaria
+  git clone $UPSTREAM_REPO /FiletrAdv_BOT
 fi
-cd /EvaMaria
+cd /FiletrAdv_BOT
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
